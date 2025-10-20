07c48da59ad8adaadd2f2ed681cc8f0847ec9dcd
andrespalvarez <andrespalvarez@hotmail.com>
2025-10-20 19:22:41 -0300

    subido validacion de errores

--- create_tables_pruebas.sql
+++ create_tables_pruebas.sql
@@ -27,18 +27,18 @@ create table if not exists Producto (
 -- --------------------------------------------------------
 -- 				PRUEBAS CON DATOS MANUALES
 -- --------------------------------------------------------
--- hay q probar insertar datos para verificar las restricciones
+-- inserciones correctas de prueba:
 use Producto_CodBarras;
 INSERT INTO CodigoBarras (id,eliminado,tipo,valor,fechaImplantacion,observaciones) VALUES
 (1,false,'EAN13', 'valorprueba1','2025-10-19', "esta es una observacion de prueba"),
 (2,false,'EAN13', 'valorprueba2','2025-10-19', "esta es una observacion de prueba"),
-(3,false,'EAN13', 'valorprueba3','2025-10-19', "esta es una observacion de prueba"),
+(3,false,'UPC', 'valorprueba3','2025-10-19', "esta es una observacion de prueba"),
 (4,false,'EAN13', 'valorprueba4','2025-10-19', "esta es una observacion de prueba"),
-(5,false,'EAN13', 'valorprueba5','2025-10-19', "esta es una observacion de prueba"),
-(6,false,'EAN13', 'valorprueba6','2025-10-19', "esta es una observacion de prueba"),
-(7,false,'EAN13', 'valorprueba7','2025-10-19', "esta es una observacion de prueba"),
-(8,false,'EAN13', 'valorprueba8','2025-10-19', "esta es una observacion de prueba"),
-(9,false,'EAN13', 'valorprueba9','2025-10-19', "esta es una observacion de prueba"),
+(5,false,'EAN8', 'valorprueba5','2025-10-19', "esta es una observacion de prueba"),
+(6,false,'EAN8', 'valorprueba6','2025-10-19', "esta es una observacion de prueba"),
+(7,false,'EAN8', 'valorprueba7','2025-10-19', "esta es una observacion de prueba"),
+(8,false,'UPC', 'valorprueba8','2025-10-19', "esta es una observacion de prueba"),
+(9,false,'EAN8', 'valorprueba9','2025-10-19', "esta es una observacion de prueba"),
 (10,false,'EAN13', 'valorprueba10','2025-10-19', "esta es una observacion de prueba");
 
 INSERT INTO Producto (id,eliminado,nombre,marca,categoria,precio, peso, codigoBarras) VALUES
@@ -53,3 +53,17 @@ INSERT INTO Producto (id,eliminado,nombre,marca,categoria,precio, peso, codigoBa
 (9,false,'producto', 'marca','categoria',150, 55, 9),
 (10,false,'producto', 'marca','categoria',150, 55, 10);
 
+-- prueba de inserciones incorrectas (utilizando updates)
+SET session sql_mode = 'STRICT_ALL_TABLES';
+-- SET SESSION sql_mode = REPLACE(@@SESSION.sql_mode, 'STRICT_ALL_TABLES', '');
+
+-- error tipo de dato:
+update Producto set id = "false" where id=2;
+update CodigoBarras set fechaImplantacion = "hola mundo" where id=1;
+
+-- error NOT NULL
+update Producto set nombre = null where id=2;
+update CodigoBarras set id = null where id=1;
+
+-- error UNIQUE
+update CodigoBarras set valor = valorprueba1 where id=2;
